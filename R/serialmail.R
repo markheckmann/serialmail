311d158ecfbb472bbd04a049957a4305221ca08b
Mark Heckmann <mark.heckmann@gmx.de>
2012-04-24 23:24:34 +0200

    first commit

---
+++ R/serialmail.R
@@ -0,0 +1,218 @@
+# generate serial mail from .xlsx or .csv file
+# parse a document and replace placeholder tags
+#
+# package depends:
+# xlsx, xlsxjars, rJava, stringr, tcltk
+# library(xlsx)  
+# library(tcltk)
+# library(stringr) 
+
+# 1) read template
+# 2) get template tags
+# 3) read data file
+# 4) check if all tags have values
+# 5) generate new text strings
+# 6) output text to file, prepare email etc.
+
+# get file suffix 
+#
+get_file_type <- function(file){  
+  x <- unlist(str_split(file, "[.]"))
+  tail(x, 1)
+}
+
+
+# read in .csv or .xlsx file
+# use file selection dialog if no file is supplied 
+#
+read_data_file <- function(file) 
+{
+  if (missing(file)) {                                           # open file selection menu if no file argument is supplied
+    Filters <- matrix(c("Excel > 2007 files", ".xlsx",
+                        "csv files", ".csv"),
+                        ncol=2, byrow = TRUE)
+    file <- tk_choose.files(filters = Filters, caption="Choose data file",
+                            multi=FALSE)    # returns complete path                    
+  }
+  #basename("r/text.txt")
+  filetype <- get_file_type(file)
+  if (filetype == "csv") {               # read csv, german version, i.e. ";" as seperator
+    x <- read.csv2(file)
+  } else if (filetype == "xlsx") {       # read excel spreadsheet 
+    x <- read.xlsx(file, 1)
+  } else {
+     stop("only files of type .csv and .xlxs are allowed")  
+  }    
+  x
+} 
+
+# read in .txt template file
+# use file selection dialog if no file is supplied
+#
+read_template_file <- function(file){
+  if (missing(file)) {                                           # open file selection menu if no file argument is supplied
+    Filters <- matrix(c("txt file", ".txt"),
+                        ncol=2, byrow = TRUE)
+    file <- tk_choose.files(filters = Filters, caption="Choose template file",
+                            multi=FALSE)    # returns complete path                    
+  }  
+  con <- file(file, "rt") 
+  template <- readLines(con)  
+  close(con)
+  template                  
+} 
+
+# strip of sourrounding tags (<<tag>>)
+#
+strip_tags <- function(x, intag="<<", outtag=">>"){
+  x <- str_replace_all(x, intag, "")
+  str_replace_all(x, outtag, "")  
+}
+
+
+# extract all tags used in template file
+#
+get_template_tags <- function(txt, intag="<<", outtag=">>")
+{
+  tags <- str_extract_all(txt, "[<<](.+?)[>>]")  # retrieve all tags in template 
+  unlist(tags)
+}
+
+
+# check if all tags in template file are
+# contained in data file
+#
+check_values_for_tags_in_template <- function(tags, data){
+  tags.inner <- strip_tags(tags)
+  tags.defined <- tags.inner %in% names(data) 
+  if (any(!tags.defined))
+    warning("no data for the following tags: ", 
+            paste(tags[!tags.defined], collapse=", "), 
+            call. = FALSE)
+}
+
+
+# prepare small data frames containing one line of data 
+# from data file and names of the tags as colnames
+#
+make_pairs_list <- function(x, intag="<<", outtag=">>"){
+  names(x)  <- paste(intag, names(x), outtag, sep="")  
+  lapply(1L:nrow(x), function(i) x[i, ])
+}
+
+
+# parse template file and replace tags by corresponding
+# tag data from data file
+#
+replace_tags_by_value <- function(pair, template){
+  tags <- get_template_tags(template)
+  avail.tags <- names(pair)
+  tags.exist <- tags %in% avail.tags
+  txt <- template  
+  for (tag in tags[tags.exist]){ 
+    i <- match(tag, avail.tags)
+    txt <- str_replace_all(txt, tag, pair[1, i])
+  }
+  txt 
+}
+
+
+# extract mail adress from data pair
+#
+get_email <- function(pair){
+  x <- pair[1, "<<email>>"]
+  if (is.null(x))
+    message("email adress is missing")
+  x
+}
+
+
+# prepare a single email
+#
+prepare_single_email <- function(msg, email, subject= "course feedback"){
+  create.post.new(msg, subject = subject, address = email)
+} 
+
+
+# prepare all emails using a list of parsed templates and email adresses
+# 
+prepare_all_emails <- function(msgs, emails, subject= "course feedback"){
+  message("Preparing email(s)")  
+  msgs.vec <- sapply(msgs, function(x) paste(x, collapse="\n"))   
+  emails.vec <- as.vector(unlist(emails))  
+  mapply(prepare_single_email, msg=msgs.vec, email=emails.vec, 
+         MoreArgs=list(subject=subject))  
+  invisible()
+}
+
+
+#' Creates a series of emails from a textfile template.
+#'
+#' A plain textfile (\code{.txt}) is used as a template to create emails.
+#' The textfile may contain any number of arbitrary tags of the form
+#' \code{<<tag>>}. The tenplate is parsed and the tags are replaced
+#' with values taken from a data file. The data is contained in 
+#' an Excel (\code{.xlsx}) or comma seperated value \code{.csv}. 
+#' Based on these files an email for each line of data in the data
+#' file is created using the default email program. 
+#'
+#' @section Template textfile: 
+#' The template file contains plain text and tags wrapped in braces <<tag>>.
+#' Any tag can be used as long as it appears in the data file. An example:
+#'
+#' \code{Hello <<name>>, your exam mark is <<mark>>. Best, Mark.}  
+#'
+#' @section Data file:
+#' The data file can be an Excel file. Then the tags without 
+#' sourrounding braces are the headers,
+#' i.e. contained in the first row of the Excel spreadsheet. Only the
+#' first spreadsheet is read.
+#'
+#' For comma seperated value files the case is the same. The tags 
+#' without braces are used as headers. Semicolons are used as seperators
+#' An example (\code{.csv}):
+#'
+#' \code{name; surname; mark}   
+#'
+#' \code{Mark; Heckmann; 1,3}   
+#'
+#' \code{Holger; Doering; 1,3}  
+#'  
+#' @param tfile     Path to template file. Must be a \code{.txt} file. 
+#'                  If no file is supplied a file selection dialog
+#'                  is opened.
+#' @param dfile     Path to data file. Must be a \code{.xlsx} (Excel 2007 or newer) 
+#'                  or \code{.csv} (German type csv, i.e. semicolon as separator) file. 
+#'                  If no file is supplied a file selection dialog
+#'                  is opened.
+#' @param output    String. Type of output. Default is to generate an \code{"email"}.
+#'                  Currently not used.
+#' @param subject   Subject line of email, default is \code{"subject line"}.
+#'
+#' @author    Mark Heckmann 
+#' @export  
+#' @examples \dontrun{
+#'
+#'  # download the files template.txt and data.csv from github dowload page
+#'  serialmail("template.txt", "data.csv")
+#' }
+#'
+serialmail <- function(tfile, dfile, output="email", subject="subject line")
+{ 
+  intag <- "<<"                                         # opening sins for parsed tags 
+  outtag <- ">>"                                        # closing sins for parsed tags   
+  template <- read_template_file(tfile)                 # 1) read template 
+  tags <- get_template_tags(template)                   # 2) get template tags   
+  data <- read_data_file(dfile)                         # 3) read data file  
+  check_values_for_tags_in_template(tags, data)         # 4) check if all tags have values  
+  pairlist <- make_pairs_list(data, intag, outtag)                  
+  text.parsed <- lapply(pairlist, replace_tags_by_value, template)    # 5) parse text and overwrite tags 
+  if (output == "email") {                                            # 6) prepare emails etc.    
+    emails <- lapply(pairlist, get_email)  
+    prepare_all_emails(text.parsed, emails, subject= subject)
+  }
+  invisible()
+}
+
+
+
---
+++ README.md
@@ -0,0 +1,19 @@
+# serialmail
+
+**serialmail** is an R package to create a series of emails from a text file template `(.txt)` and a data file `(.csv)` or `(.xlsx)`.
+                                                  
+To install the latest development version from github you can use the `devtools` package.
+    
+    library(devtools)
+    install_github("OpenRepGrid", "markheckmann", dependencies=TRUE) 
+
+You may need to install the following packages before you install `serialmail`:
+`xlsx, xlsxjars, rJava, stringr`´.
+
+To get started download the files from the [downloads](http://github.com/markheckmann/serialmail/downloads) page.
+Start to generate emails from the files:
+    library(serialmail)
+    serialmail("template.txt", "data.csv")     
+    # or   
+    serialmail("template.txt", "data.xlsx") 
+    
